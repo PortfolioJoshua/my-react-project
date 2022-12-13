f0cacd182e814ed073fff75ff1fa874948b6f37a
Joshua Eirman <joshuaeirm@gmail.com>
2022-12-13 18:37:55 -0500

    first commit - styled-components - word is blue

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,17 @@
+import { Fragment} from 'react';
+import { NavigationContainer, } from './navigation.styles.jsx';
+
+
+const Navigation = () => {
+return (
+<Fragment>
+<NavigationContainer>
+<div>
+test
+</div>
+</NavigationContainer>
+</Fragment>
+);
+};
+
+export default Navigation;
